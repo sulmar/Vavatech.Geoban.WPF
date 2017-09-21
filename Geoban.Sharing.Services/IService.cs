69e312903d236247d8f3932c6fe9976919f257a7
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-21 19:54:32 +0200

    Utworzenie generycznej klasy usługi

---
+++ Geoban.Sharing.Services/IService.cs
@@ -0,0 +1,32 @@
+﻿using Geoban.Sharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geoban.Sharing.Services
+{
+    public interface IService<TItem>
+    {
+        IList<TItem> Get();
+
+        TItem Get(int id);
+
+        void Add(TItem item);
+
+        void Update(TItem item);
+
+        void Remove(int id);
+    }
+
+
+    public interface ICustomersService : IService<Customer>
+    {
+    }
+
+    public interface IRentalsService : IService<Rental>
+    {
+
+    }
+}
