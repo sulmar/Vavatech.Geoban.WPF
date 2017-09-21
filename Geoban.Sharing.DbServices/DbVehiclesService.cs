e329f005d31f3ddff38a2f5011d4e9364d14052b
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-21 19:52:26 +0200

    Utworzenie implementacji bazy danych

---
+++ Geoban.Sharing.DbServices/DbVehiclesService.cs
@@ -0,0 +1,38 @@
+﻿using Geoban.Sharing.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geoban.Sharing.Models;
+
+namespace Geoban.Sharing.DbServices
+{
+    public class DbVehiclesService : IVehiclesService
+    {
+        public void Add(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Vehicle> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
