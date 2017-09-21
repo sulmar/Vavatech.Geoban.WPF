6ca4b1370410b5308e48bf073e75a8921ca49eff
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-21 10:27:03 +0200

    Utworzenie interfejsów

---
+++ Geoban.Sharing.Services/IVehiclesService.cs
@@ -0,0 +1,22 @@
+﻿using Geoban.Sharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geoban.Sharing.Services
+{
+    public interface IVehiclesService
+    {
+        IList<Vehicle> Get();
+
+        Vehicle Get(int id);
+
+        void Add(Vehicle vehicle);
+
+        void Update(Vehicle vehicle);
+
+        void Remove(int id);
+    }
+}
