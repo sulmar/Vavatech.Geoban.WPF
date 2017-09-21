3fca7c9a08e1442a0cbec1517da48870b4b29ee7
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-21 10:27:33 +0200

    Utworzenie klienta WPF

---
+++ Geoban.Sharing.WPFClient/ViewModels/VehiclesViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Geoban.Sharing.MockServices;
+using Geoban.Sharing.Models;
+using Geoban.Sharing.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geoban.Sharing.WPFClient.ViewModels
+{
+    public class VehiclesViewModel : ViewModelBase
+    {
+        public IList<Vehicle>  Vehicles { get; set; }
+
+        private IVehiclesService vehiclesService;
+
+        public VehiclesViewModel()
+        {
+            vehiclesService = new MockVehiclesService();
+
+            Load();
+        }
+
+        public void Load()
+        {
+            Vehicles = vehiclesService.Get();
+        }
+
+
+
+    }
+}
